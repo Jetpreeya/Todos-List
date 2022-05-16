eabcb7af47e2a1e1b155242431b75b4312b9287c
Jetpreeya <Jetpreeya.Norkaew@gmail.com>
2022-05-16 16:36:04 +0200

    Todo list begin

---
+++ myreactproject/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./components/Header";
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="app-wrapper">
+        <div>
+          <Header/>
+        </div>
+      </div>      
+    </div>
+  );
+}
+
+export default App;
