e01265b847db603026bb4b25bd3ad7370fef1c7c
Jetpreeya Yawichai <84719740+Jetpreeya@users.noreply.github.com>
2022-05-18 11:55:25 +0200

    Todos list is done.

--- myreactproject/src/App.js
+++ myreactproject/src/App.js
@@ -1,12 +1,18 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./components/Header";
 import Form from "./components/Form";
 import TodosList from "../src/components/TodosList"
 import './App.css';
 
 const App = () => {
+  const initialState = JSON.parse(localStorage.getItem("todos")) || []; 
   const[input, setInput] = useState("");
-  const[todos, setTodos] = useState([]);
+  const[todos, setTodos] = useState(initialState);
+  const[editTodo, setEditTodo] = useState(null);
+
+  useEffect(() =>{
+    localStorage.setItem("todos", JSON.stringify(todos));
+  })
 
   return (
     <div className="container">
@@ -18,9 +24,12 @@ const App = () => {
           setInput={setInput}
           todos={todos}
           setTodos={setTodos}
+          editTodo={editTodo}
+          setEditTodo={setEditTodo}
           />
         </div>
-        <div><TodosList todos={todos} setTodos={setTodos}/>
+        <div>
+          <TodosList todos={todos} setTodos={setTodos} setEditTodo={setEditTodo}/>
         </div>
       </div>      
     </div>
--- myreactproject/src/components/Form.js
+++ myreactproject/src/components/Form.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {v4 as uuidv4} from "uuid";
 
-const Form = ({input, setInput, todos, setTodos}) => {
+const Form = ({input, setInput, todos, setTodos, editTodo, setEditTodo}) => {
+    const updateTodo = (title, id, completed) =>{
+        const newTodo = todos.map((todo) =>
+            todo.id === id ? {title, id, completed} : todo
+        );
+        setTodos(newTodo);
+        setEditTodo("");
+    };
+    useEffect(() =>{
+        if(editTodo){
+            setInput(editTodo.title);
+        } else{
+            setInput("")
+        }
+    }, [setInput, editTodo]);
+
     const onInputChange = (event) =>{
         setInput(event.target.value);
     };
+
     const onFormSubmit = (event) =>{
         event.preventDefault();
-        setTodos([...todos, {id: uuidv4(), title: input, completed:false}]);
+        if(!editTodo){
+           setTodos([...todos, {id: uuidv4(), title: input, completed:false}]);
+        setInput(""); 
+        } else{
+            updateTodo(input, editTodo.id, editTodo.completed)
+        }
     };
     return (
         <form onSubmit={onFormSubmit}>
@@ -15,9 +36,9 @@ const Form = ({input, setInput, todos, setTodos}) => {
             value={input}
             required
             onChange={onInputChange}/>
-            <button className="button-add" type="submit"> Add</button>
+            <button className="button-add" type="submit"> {editTodo ? "OK" : "Add"}</button>
         </form>
-    )
-}
+    );
+};
 
 export default Form;
